01157d1ce1eb1acca0ed8bb8aa43e3fc9164b4ea
greck <d.popov@yandex.by>
2026-04-01 14:31:44 +0300

    [fix]

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,32 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        // local dev
+        'http://localhost:3000',
+        'http://localhost:5173',
+
+        // vercel preview
+        /\.vercel\.app$/,
+
+        // future production domains
+        'https://agraris.ru',
+        'https://www.agraris.ru',
+
+        'https://agraris.tech',
+        'https://www.agraris.tech',
+
+        'https://agraristech.by',
+        'https://www.agraristech.by',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
